00037847af79a0edc9389822f06169bf1d14fe9a
pradbose <pradbose@gmail.com>
2013-04-30 04:03:55 -0500

    Update StructuralSimilarity.py

--- Src/StructuralSimilarity.py
+++ Src/StructuralSimilarity.py
@@ -1,28 +1,74 @@
+import urllib
+from lxml import html
+from bs4 import BeautifulSoup
+from collections import defaultdict
+import re
+import random
+
 class StructuralSimilarity:
-	def __init__(self):
-		return
-	
-	
-	''' takes a web page as input and strips everything except the supplied tag'''
-	def parsePageSeparateTag(self,webPageContent,tagName):
-		
-		return
-	
-	''' Represent the tag in the doc in a string form'''
-	def getStringRepresentationOfTag(self,tagName):
-		return
-	
-	'''take values from Constants file to calculate the distance measure of the two supplied string representatuions''' 
-	def getDistanceValueFromString(self,stringRep1,stringRep2):
-		return 
-	
-	'''loops over all the tags in the two web pages.
-	for each tag that appears in the pages calculate the distance value of that tag.
-	get all such distance values and calculte a final distance value '''
-	def getStructuralDiffOfPages(self,webPage1,webPage2):
-		return
-		
-		
-		
-		
-		
+    
+    def __init__(self):
+        self.urldict={}
+        self.taglist=self.makeTaglist("../Resources/htmltags.txt")
+        num=len(self.taglist)
+        self.tagweights=self.makeTagweights(self.taglist, num)
+        return
+    
+    def makeTagweights(self,tags,number):
+        tweights={}
+        for tag in tags:
+            tweights[tag]=float(0.05)
+        return tweights
+    
+    def makeTaglist(self,file):
+        tlist=[]
+        f = open("C:\Users\sanjekus\workspace\webiso\Resources\htmltags.txt", "r")
+        lines = [line.strip() for line in f]
+        for content in lines:
+            mySubString=content[content.find("<")+1:content.find(">")]
+            tlist.append(mySubString)
+        return tlist
+    
+    def readURL(self,url):
+        self.urldict[url]=defaultdict(int)
+        pg=urllib.urlopen(url).read()
+        soup = BeautifulSoup(pg)
+        for tag in self.taglist:
+            self.urldict[url][tag]=len(soup.find_all(tag))
+        return
+    
+    def computeDist(self,url1,url2):
+        dist=0
+        for tag in self.taglist:
+            diff=self.urldict[url1][tag]-self.urldict[url2][tag]
+            dist+=diff*diff*(self.tagweights[tag])
+        return dist
+    
+    def compareURL(self,url1,url2):
+        self.readURL(url1)
+        self.readURL(url2)
+        dis=self.computeDist(url1, url2)
+        return dis
+    
+    def main(self, url1, url2):
+        distance = self.compareURL(url1, url2)
+        distance = float(float(distance)/40000.0)
+        distance = float(1.0 - distance)
+        distance = abs(distance)
+        rand = 0.0
+        if distance > 1.0:
+            rand = random.uniform(0.99, 1.0)
+            distance = rand
+        return distance
+
+  
+if __name__ == '__main__':
+    structureSim = StructuralSimilarity()
+    url1 = 'http://www.google.com'
+    url2 = 'http://www.rahulgandhiachievements.com/'
+    distance = structureSim.main(url1, url2)
+    
+    print distance
+
+    
+    
